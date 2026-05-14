a257dcbfb4573d538eb8c8dd996d3c2864a7ff32
Orphan-Obliterator <87661305+Orphan-Obliterator@users.noreply.github.com>
2026-05-14 22:14:53 +0100

    Wood_White_V2
    improved wood white model!!!!!

---
+++ Wood_White_model.py
@@ -0,0 +1,69 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+# This is a new model that uses 5 parameters, V_X, R_X, k, n and l
+# V_X is the maximum velocity, or the velocity at the boundary (either that of the eye/eye wall)
+# R_X is the radial distance at which said maximum velocity is attained 
+# find out about k, n and l in the onenote!!!!
+# f is the coriolis parameter 
+
+
+# for conversion into cartesian later 
+
+x = np.linspace(-10, 10, 40)
+y = np.linspace(-10, 10, 40)
+X, Y = np.meshgrid(x, y)
+
+r = np.sqrt(X**2 + Y**2)
+
+
+theta = np.arctan2(Y, X)
+
+
+# defining paramters ()
+R_1X = 3
+n_1 = 4
+l_1 = 0.01
+f = 2 
+k_1 = 2
+V_1X = 9
+p_1 = r/R_1X
+
+
+R_1CX = V_1X/(f * R_1X)
+
+phi_1 = ((n_1 ** l_1) * (p_1 ** k_1)) * (1/(n_1 - k_1 + k_1 * p_1 ** (n_1/l_1))) ** l_1 
+
+V = (V_1X) * phi_1 
+
+
+U = -V * np.sin(theta)
+W =  V * np.cos(theta)
+
+plt.quiver(
+     X, Y,
+     U, W,
+     V,                  # colour by magnitude
+    scale= 2,            # smaller = bigger arrows
+     scale_units='xy',
+     angles='xy'
+    )
+
+plt.xlim(0, 9)
+plt.ylim(0,8)
+
+plt.xlabel("x")
+
+plt.ylabel("y")
+
+plt.title("Cyclone Vector Field")
+
+plt.colorbar(label="Velocity Magnitude")
+
+plt.axis("equal")
+plt.show()
+
+#plt.plot(r, V)
+#plt.show()
+
+
+
