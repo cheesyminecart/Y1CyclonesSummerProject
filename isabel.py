bc2bfa71fa895eddca54c07e30f0c229dc63b979
JackSkidoo <jackmaxfield523@gmail.com>
2026-05-22 10:28:22 +0100

    Add files via upload

---
+++ isabel.py
@@ -0,0 +1,22 @@
+import datetime as dt
+from tropycal import tracks
+
+# Load HURDAT2
+basin = tracks.TrackDataset(basin='north_atlantic', include_btk=False)
+
+# Get the Hurricane Isabel data
+isabel = basin.get_storm(('isabel', 2003))
+
+
+t = dt.datetime(2003, 9, 18, 18)
+
+# Find index
+idx = isabel.dict['time'].index(t)
+
+# Find the coordinates of the centre of the cyclone
+lat = isabel.dict['lat'][idx]
+lon = isabel.dict['lon'][idx]
+
+print("Isabel centre at 18 Sep 2003 18:00 UTC:")
+print("Latitude:", lat)
+print("Longitude:", lon)
